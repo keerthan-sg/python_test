932a94bcae78932838b90b92e43f98b72cd60c43
unknown <keerthansp11@gmail.com>
2023-05-26 12:04:34 +0530

    first commit

---
+++ word_replacement_program.py
@@ -0,0 +1,9 @@
+def word_replacer():
+    print("enter word to repalce in string")
+    str="hi this is hi dd"
+    word_to_replace=input("Enter word to replace")
+    word_repalcement=input("Enter word replacement")
+
+    print(str.replace(word_to_replace,word_repalcement))
+
+word_replacer()
